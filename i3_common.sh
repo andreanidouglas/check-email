0c3443c82ae6bd90372bcab9e6c16a6061abfec6
Douglas R Andreani <andreani.dr@gmail.com>
2024-02-24 17:31:48 -0300

    Update i3_common.sh

--- i3_common.sh
+++ i3_common.sh
@@ -4,11 +4,11 @@ i3status | while :
 do
 
 	read line
-	emails=$(/home/cmte/r/check-emails/check-emails /home/cmte/r/check-emails/email.yml 2> /dev/null)
+	emails=$(./check-emails ./email.yml 2> /dev/null)
 	if [ $emails -eq 0 ]; then
 		echo $line || exit 1
 	else
-		echo "GMAIL: Unread ("$(/home/cmte/r/check-emails/check-emails /home/cmte/r/check-emails/email.yml 2> /dev/null)") | $line" || exit 1
+		echo "GMAIL: Unread ("$emails") | $line" || exit 1
 	fi
 
 done
